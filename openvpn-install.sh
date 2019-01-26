4c148f37a998d15789a2260d3b8dc4efc463c453
mr-bolle <mr-bolle@users.noreply.github.com>
2019-01-26 23:54:16 +0100

    Adjustment of container IP eliminated

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -26,8 +26,6 @@ export OVPN_DATA
 
 sleep 1
 
-echo -e "** Note: you can select the default [Value default] with hit enter **\n"
-
 # OpenVPN dynDNS Domain (ex vpn.example.com:443)
 read -p "Please enter your dynDNS Addess: " IP
 
@@ -42,49 +40,21 @@ read -p "Please choose your Protocol (tcp / [udp])?: " PROTOCOL
         echo -e "\n  Your Domain is: $PROTOCOL://$IP \n"
     fi
 
-# Pi-Hole Host-IP set to docker-compose as ServerIP Envi
-#  Work in Progress - no adjstment possible
+# Pi-Hole Host-IP set to docker-compose as ServerIP Environment
 
-# set the Host-IP for the Pi-Hole SERVERIP
+# read current ServerIP
 HostIP=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
-echo "SERVER_IP=$HostIP" > .env         # create each run a new file
 
     # change HostIP
-
-#    sed -r ServerIP/s/HostIP-PiHole/192.168.178.29/p docker-compose.yml
-
-            HostCompose=`grep ServerIP docker-compose.yml | awk '{ print $2 }'`
-        if [ -z $HostCompose != $HostIP]; then
-                # docker-compose Pihole Host not equal HostIP
-
-            else
-            
+    HostIP_OLD=`grep 'ServerIP' docker-compose.yml | awk '{print $2}'`
+        if [ $HostIP_OLD != $HostIP ]; then
+                # docker-compose Pihole ServerIP not equal HostIP
+                sed -in "/ServerIP/s/$HostIP_OLD/$HostIP/g" docker-compose.yml        # search for ServerIP and replace HostIP
+            #else
+                #echo "HostIP same, change is not necessary"
         fi
 
-
-            # if [ "$PIHOLE_PASSWORD_NEW" != $PIHOLE_PASSWORD_OLD ]; then
-            #     sed -in "/WEBPASSWORD/s/$PIHOLE_PASSWORD_OLD/$PIHOLE_PASSWORD_NEW/g" docker-compose.yml        # search for WEBPASSWORD and replace this Password
-            #     PIHOLE_PASSWORD_now=`grep 'WEBPASSWORD' docker-compose.yml | awk '{print $2}'`
-
-
-# change Pi-Hole Container IP
-read -p "Please enter the Pi-Hole Container IP [default 172.110.1.4]: " PIHOLEIP
-    PIHOLEIP=${PIHOLEIP:-'172.110.1.4'}   # set the default IP (if user skip this entry)
-
-    if [[ $PIHOLEIP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    echo -e "\n  This is a valid IP: $PIHOLEIP"
-    else
-        echo -e "\n*****************************************************\n"
-        echo -e "\n  This is a invalid IP: "$PIHOLEIP", please try again!"
-        echo -e "\n*****************************************************\n"
-        sleep 3 && exit
-    fi
-
-# change OpenVPN Container IP
-
-
 # set the Pi-Hole Web Admin Password
-
 # read current PiHole Admin Password from docker-compose.yml
 PIHOLE_PASSWORD_OLD=`grep 'WEBPASSWORD' docker-compose.yml | awk '{print $2}'`
 
@@ -115,9 +85,43 @@ read -p "Would you change the Pi-Hole Admin Password? [default $PIHOLE_PASSWORD_
 
             fi
 
+
+# Pi-Hole & OpenVPN Container IP set to docker-compose
+
+# # read current Container IP
+
+##  ** stop this idea  **
+#     OpenVPN_IP_OLD=`grep 'ipv4' docker-compose.yml | awk ' NR==1 {print $2}'`      # first IPv4
+#     OpenVPN_IP_OLD_LINE=`grep --line-number 'ipv4' docker-compose.yml | awk ' NR==1 {print $1}' |  sed 's/[^0-9]*//g'`  # first result from ipv4 return linenumber (without :)
+
+#     PIHOLE_IP_OLD=`grep 'ipv4' docker-compose.yml | awk ' NR==2 {print $2}'`       # secont IPv4
+#     PIHOLE_IP_OLD_LINE=`grep --line-number 'ipv4' docker-compose.yml | awk ' NR==2 {print $1}' | sed 's/[^0-9]*//g'`  # first result from ipv4 return linenumber (without :)
+
+# read -p "Please enter the Pi-Hole Container IP [default $PIHOLE_IP_OLD]: " PIHOLE_IP
+#     PIHOLE_IP=${PIHOLE_IP:-$PIHOLE_IP_OLD}   # set the default IP (if user skip this entry)
+
+#     if [[ $PIHOLE_IP != ^[10|172|192].[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+#    # if [[ $PIHOLE_IP != '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' ]]; then
+#     #    echo -e "\n  This is a valid IP: $PIHOLE_IP  Line: $PIHOLE_IP_OLD_LINE $PIHOLE_IP"
+
+#         sed -in "${PIHOLE_IP_OLD_LINE}s/${PIHOLE_IP_OLD}/${PIHOLE_IP}/g" docker-compose.yml  # replace replacing-string-based-on-line-number
+#         echo `grep 'ipv4' docker-compose.yml | awk ' NR==2 {print $2} '`
+
+# #        sed -in "/ServerIP/s/$HostIP_OLD/$HostIP/g" docker-compose.yml
+#     else
+#         echo -e "\n*****************************************************\n"
+#         echo -e "\n  This is a invalid IP: "$PIHOLE_IP", please try again!"
+#         echo -e "\n*****************************************************\n"
+#         sleep 3 && exit
+#     fi
+
+
 #Step 2
 echo -e "\nStep 2\n"
-docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -n $PIHOLEIP -u $PROTOCOL://$IP 
+
+# read IPv4 from Pi-Hole Container 
+PIHOLE_IP=`grep 'ipv4' docker-compose.yml | awk ' NR==2 {print $2}'`
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -n $PIHOLE_IP -u $PROTOCOL://$IP 
 # more Option: https://github.com/kylemanna/docker-openvpn/blob/master/bin/ovpn_genconfig
 
 
@@ -130,24 +134,6 @@ echo -e "\nWe are now at Step 3\n"
 
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
 
-
-
-#Step 4
-## nicht notwendig, da der container später gestartet wird
-#sleep 1
-#echo -e "\nStep 4, We are Starting OpenVPN server process please wait ...\n"
-
-# docker run --name vpn_openvpn -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
-
-#sleep 3
-
-#echo "\nSee I am up and running Alhumdulliah\n"
-
-#docker ps -a
-
-#echo -e "\nMy name is OpenVPN, I am running inside the container name OpenVPN\n"
-
-#sleep 3
 
 #Step 5
 echo -e "\nWe are now at 5th Step, Generate a client certificate with  a passphrase SAME AS YOU GIVE FOR SERVER...PASSPHRASE please wait...\n"
@@ -162,7 +148,6 @@ docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-c
 
 #Step 6
 echo -e "\nWe are now at 6TH Step, don't worry this is last step, you lazy GUY,Now we retrieve the client configuration with embedded certificates\n"
-
 
 echo -e "\n$CLIENTNAME ok\n"
 
@@ -185,7 +170,5 @@ cp $PWD/$CLIENTNAME.ovpn $OVPN_DATA
 docker network inspect vpn-net &>/dev/null || 
     docker network create --driver=bridge --subnet=172.110.1.0/24 --gateway=172.110.1.1 vpn-net
 
-# docker network ls|grep my_local_network > /dev/null || docker network create --driver bridge my_local_network
-
 # run docker-compose
-docker-compose up -d -f docker-compose.yml
+docker-compose up -d
